0fd4ec335f33b53c02561bd79a5fe053f780cda3
Kazuki Ota <ota_droid@live.jp>
2023-01-30 17:18:17 +0900

    fix ns declare.

--- src/percent.hpp
+++ src/percent.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
-void PERCENT::encode(const char* input, char* output);
-size_t PERCENT::encodeLength(const char* input);
-void PERCENT::decode(const char* input, char* output);
-size_t PERCENT::decodeLength(const char* input);
+namespace PERCENT{
+    void encode(const char* input, char* output);
+    size_t encodeLength(const char* input);
+    void decode(const char* input, char* output);
+    size_t decodeLength(const char* input);
+}
